0dcb0e3cf6f0ce8e66d193e7670c37e370e7a39b
Juanshep1 <shephard_juan@yahoo.com>
2025-12-16 22:34:57 -0600

    Initial Commit

---
+++ Rubicon/RubiconApp.swift
@@ -0,0 +1,17 @@
+//
+//  RubiconApp.swift
+//  Rubicon
+//
+//  Created by Juan Shephard on 12/16/25.
+//
+
+import SwiftUI
+
+@main
+struct RubiconApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RubiconTests/RubiconTests.swift
@@ -0,0 +1,17 @@
+//
+//  RubiconTests.swift
+//  RubiconTests
+//
+//  Created by Juan Shephard on 12/16/25.
+//
+
+import Testing
+@testable import Rubicon
+
+struct RubiconTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
